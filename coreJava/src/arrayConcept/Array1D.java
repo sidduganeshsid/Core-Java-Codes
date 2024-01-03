e71633f78035003d31f842be0c26f8510e803671
sidduganeshsid <sidduganeshengineer@gmail.com>
2024-01-03 10:46:17 +0530

    array

---
+++ coreJava/src/arrayConcept/Array1D.java
@@ -0,0 +1,11 @@
+package arrayConcept;
+
+public class Array1D {
+
+	public static void main(String[] args) {
+		int ar[] = {23,25,34,5,4};
+		for(int elem : ar)
+			System.out.println(elem+" ");
+	}
+
+}
